35d41e2a8228e6a812ff804afa344a20171105d3
Cameron Kidman <cameronjkidman@gmail.com>
2015-11-15 16:42:47 -0700

    First commit of the CW-redo. Added basic api stuff and routes

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,9 @@
+class ::Api::V1::UsersController < ActionController::Api
+  def create
+
+  end
+
+  def dashboard
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, :only => [:create] do
+        get :dashboard
+      end
+    end
+  end
+end
