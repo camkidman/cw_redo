b0ffb446b3be8f7c32001b94d4f22327c3006a9a
Cameron Kidman <cameronjkidman@gmail.com>
2015-11-16 18:25:53 -0700

    Route support for api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class ::Api::V1::UsersController < ActionController::Api
+  respond_to :json
   def create
 
   end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    name "Cam Kidman"
+    email "cam@cw.com"
+  end
+end
