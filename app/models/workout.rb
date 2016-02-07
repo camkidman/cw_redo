eb83bdf4feba9be63f6b18bb5b19546301de9a15
Cameron Kidman <cameronjkidman@gmail.com>
2016-02-06 23:18:14 -0700

    Add a check to generate workouts if it's an initial test workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,11 +3,19 @@ class Workout < ActiveRecord::Base
   belongs_to :initial_test
   has_many :exercises
   has_many :exercise_details, through: :exercises
+  after_update :generate_additional_workouts
 
   accepts_nested_attributes_for :exercise_details
 
   def mark_complete_if_so!
     return unless exercise_details.all? { |ed| ed.exercise_complete? }
     self.update_attribute :complete, true
+  end
+
+private
+  def generate_additional_workouts
+    if initial_test_id.present? && complete?
+      ::WorkoutGeneration::WorkoutGenerator.generate_workouts(initial_test)
+    end
   end
 end
